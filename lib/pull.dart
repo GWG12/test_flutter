b760537aaa89042db1f1dfb05cffb8317e8a2a09
Gilberto Walle <gwalle.gza@gmail.com>
2020-03-31 23:49:18 -0600

    pull

---
+++ lib/pull.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class Pull extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
